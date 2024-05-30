68c3071aff111ab910452b57a55ac3c680f5cc37
Buddheswar2299 <168357487+Buddheswar2299@users.noreply.github.com>
2024-05-30 21:46:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const hideBtn = document.getElementById('hide-btn')
 const deskImg = document.getElementById('desk-img')
+const showbtn = document.getElementById('show-btn')
+console.log(showbtn)
 console.log(hideBtn)
 console.log(deskImg)
 
@@ -9,4 +11,10 @@ console.log(deskImg)
 hideBtn.addEventListener('click',function(){
     deskImg.style.visibility = 'hidden'
     console.log("hidden")
+})
+
+showbtn.addEventListener('click',function(){
+    deskImg.style.visibility = 'visible'
+    console.log('show')
+
 })
